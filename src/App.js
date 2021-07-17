5f39c1a7b4990bdce4d51a31664f9ac29445e85a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-17 11:25:17 -0500

    Set up a default property for dropdown and move Dropdown component to its own file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React, { useState } from 'react'
+import Dropdown from './components/Dropdown'
 import './App.css'
 
-const Dropdown = ({ options, id, onSelectedValueChange }) => (
-  <select name="pets" id={id} onChange={(event) => onSelectedValueChange(event.target.value)}>
-    {/* <option value="">--Please choose an option--</option> */}
-    {options.map(({ value, label }) => (
-      <option id={value} value={value} key={value}>
-        {label}
-      </option>
-    ))}
-  </select>
-)
-
 const options = [
-  { value: 'dog', label: 'dog' },
-  { value: 'cat', label: 'cat' },
-  { value: 'hamster', label: 'hamster' },
-  { value: 'parrot', label: 'parrot' },
-  { value: 'spider', label: 'spider' },
-  { value: 'goldfish', label: 'goldfish' }
+  { value: 'dog', label: 'Dog' },
+  { value: 'cat', label: 'Cat' },
+  { value: 'hamster', label: 'Hamster' },
+  { value: 'parrot', label: 'Parrot' },
+  { value: 'spider', label: 'Spider' },
+  { value: 'goldfish', label: 'Goldfish' }
 ]
+
+const initialValue = 'hamster'
 
 const App = () => {
   // state is a good way to keep track of the selected option
-  const [selectedValue, setSelectedValue] = useState()
+  const [selectedValue, setSelectedValue] = useState(initialValue)
   console.log(selectedValue)
 
   return (
     <div>
       <label htmlFor="pet-select">Choose a pet:</label>
-      <Dropdown options={options} id="pet-select" onSelectedValueChange={setSelectedValue} />
+      <Dropdown //
+        options={options}
+        selectedValue={selectedValue}
+        onSelectedValueChange={setSelectedValue}
+        id="pet-select"
+      />
     </div>
   )
 }
