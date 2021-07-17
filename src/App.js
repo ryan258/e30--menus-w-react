75a91fe03c2caeac7b42141b0a087352b0429561
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-17 10:34:18 -0500

    init project starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import { range } from 'd3'
+import { Face } from './components/Face'
+
+const width = 166
+const height = 166
+
+// console.log(arc)
+// const array = [1, 2, 3, 4, 5, 6]
+const array = range(20)
+
+const App = () => <></>
+
+export default App
